94de9a703c1a08303b09c10f20eee73a6f66ae08
xuan <xuan@LAPTOP-5DH39D1R>
2024-08-20 02:41:25 +0800

    Merge origin/main into main
    # Conflicts:
    #	src/assignment/Product.java
    #	src/assignment/main.java

--- src/boundary/DoneeManagementUI.java
+++ src/boundary/DoneeManagementUI.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package boundary;
-import entity.Donee;
+
+import entity.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 /**
@@ -11,39 +14,99 @@ import java.util.Scanner;
  * @author Yunshen
  */
 public class DoneeManagementUI {
+
     Scanner scanner = new Scanner(System.in);
 
-  public int getMenuChoice() {
-   try{
-    System.out.println("\n******DONEE MANAGEMENT******");
-    System.out.println("1. Add New Donee");
-    System.out.println("2. Update Donee Details");
-    System.out.println("3. Search Donee Details");
-    System.out.println("4. List Donee Donations");
-    System.out.println("5. Filter Donee");
-    System.out.println("6. Remove Donee");
-    System.out.println("7. Generate Summary Report");
-    System.out.println("0. Quit");
-    System.out.print("Enter choice(0-7): ");
-    int choice = scanner.nextInt();
-    scanner.nextLine();
-    System.out.println();
-    return choice;
-   }catch(Exception ex){
-       System.out.println("Invalid input");
-   }
-    return 0;
-  }
-   public void listAllDonees(String outputStr) {
-    System.out.println("\nList of Products:\n" + outputStr);
-  }
-   
-//   public Donee inputProductDetails() {
-//    String productCode = inputProductCode();
-//    String productName = inputProductName();
-//    int quantity = inputQuantity();
-//    System.out.println();
-//    return new Product(productCode, productName, quantity);
-//  }
+    public int getMenuChoice() {
+        try {
+            System.out.println("\n******DONEE MANAGEMENT******");
+            System.out.println("1. Add New Donee");
+            System.out.println("2. Update Donee Details");
+            System.out.println("3. Search Donee Details");
+            System.out.println("4. List Donee Donations");
+            System.out.println("5. Filter Donee");
+            System.out.println("6. Remove Donee");
+            System.out.println("7. Generate Summary Report");
+            System.out.println("0. Quit");
+            System.out.print("Enter choice(0-7): ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
+            return choice;
+        } catch (Exception ex) {
+            System.out.println("Invalid input");
+        }
+        return 0;
+    }
+
+    public void listAllDonees(String outputStr) {
+        System.out.println("\nList of Donee:\n" + outputStr);
+    }
+
+    public String inputDoneeID() {
+        System.out.print("Enter Donee ID: ");
+        String id = scanner.nextLine();
+        return id;
+    }
+
+    public String inputDoneeType() {
+        System.out.print("Enter Donee Type(Organisation/Family/Individual): ");
+        String type = scanner.nextLine();
+        return type;
+    }
+
+    public String inputDoneeName() {
+        System.out.print("Enter Donee Name: ");
+        String name = scanner.nextLine();
+        return name;
+    }
+
+    public String inputDoneeEmail() {
+        System.out.print("Enter Donee Email: ");
+        String email = scanner.nextLine();
+        return email;
+    }
+
+    public int inputPhoneNum() {
+        System.out.print("Enter Phone Number: ");
+        int phone = scanner.nextInt();
+        scanner.nextLine();
+        return phone;
+    }
+
+    public double inputDonationAmount() {
+        System.out.print("Enter Donation Amount:RM ");
+        int amount = scanner.nextInt();
+        scanner.nextLine();
+        return amount;
+    }
+
+    public LocalDate inputDonationDate() {
+        System.out.print("Enter the date (YYYY-MM-DD): ");
+        String inputDate = scanner.nextLine();
+
+        // Define the desired date format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date = LocalDate.parse(inputDate, formatter);
+//    try {
+//        
+//        System.out.println("You entered: " + date);
+//    } catch (Exception e) {
+//        System.out.println("Invalid date format. Please use YYYY-MM-DD.");
+//    }
+        return date;
+    }
+
+    public Donee inputDoneeDetails() {
+        String id = inputDoneeID();
+        String type = inputDoneeType();
+        String name = inputDoneeName();
+        int phone = inputPhoneNum();
+        String email = inputDoneeEmail();
+        double amount = inputDonationAmount();
+        LocalDate date = inputDonationDate();
+        System.out.println();
+        return new Donee(id, type, name, phone, email, amount, date);
+    }
 
 }
