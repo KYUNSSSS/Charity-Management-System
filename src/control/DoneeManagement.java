0a052a594479aa9374da380c2d29751515c95c64
xuan <xuan@LAPTOP-5DH39D1R>
2024-08-20 02:41:19 +0800

    Merge origin/main into main
    # Conflicts:
    #	src/assignment/Product.java
    #	src/assignment/main.java

--- src/control/DoneeManagement.java
+++ src/control/DoneeManagement.java
@@ -3,15 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package control;
-import boundary.DoneeManagementUI;
-import utility.MessageUI;
+import dao.*;
+import adt.*;
+import boundary.*;
+import utility.*;
 import entity.Donee;
 /**
  *
  * @author Hp
  */
 public class DoneeManagement {
+    private ListInterface<Donee> doneeList = new LinkedList<>();
+    private DoneeDAO doneeDAO = new DoneeDAO();
     private DoneeManagementUI doneeUI = new DoneeManagementUI();
+    
+    public DoneeManagement() {
+    doneeList = doneeDAO.retrieveFromFile();
+  }
+    
     public void runDoneeManagement() {
         int choice = 0;
         do {
@@ -21,12 +30,12 @@ public class DoneeManagement {
               MessageUI.displayExitMessage();
               break;
             case 1:
-              //addNewDonee();
-             // doneeUI.listAllDonee(getAllDonee());
+              addNewDonee();
+              doneeUI.listAllDonees(getAllDonee());
               break;
             case 2:
               //updateDoneeDetails();
-              //doneeUI.listAllDonees(getAllDonee());
+              doneeDAO.retrieveFromFile();
               break;
              case 3:
              // searchDoneeDetails();
@@ -50,15 +59,22 @@ public class DoneeManagement {
       }
         
     public void addNewDonee() {
-    Donee newProduct = DoneeManagementUI.inputDoneeDetails();
-    productList.add(newProduct);
-    productDAO.saveToFile(productList);
+    Donee newDonee = doneeUI.inputDoneeDetails();
+    doneeList.add(newDonee);
+    doneeDAO.saveToFile(getAllDonee());
   }
-    public void updateNewProduct() {
-    Donees newProduct = DoneeManagementUI.inputProductDetails();
-    productList.add(newProduct);
-    productDAO.saveToFile(productList);
+    public String getAllDonee() {
+    String outputStr = "";
+    for (int i = 1; i <= doneeList.getNumberOfEntries(); i++) {
+      outputStr += doneeList.getEntry(i) + "\n";
+    }
+    return outputStr;
   }
+//    public void updateNewProduct() {
+//    Donee newProduct = DoneeManagementUI.inputDoneeDetails();
+//    productList.add(newProduct);
+//    productDAO.saveToFile(productList);
+//  }
       
     public static void main(String[] args) {
     DoneeManagement doneeMaintenance = new DoneeManagement();
