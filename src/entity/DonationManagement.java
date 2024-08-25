8e17efb6db0d50ae0ff87411d82e944918fd1ced
lohhj <121841523+lohhj@users.noreply.github.com>
2024-08-25 16:27:46 +0800

    Update DonationManagement.java

--- src/entity/DonationManagement.java
+++ src/entity/DonationManagement.java
@@ -4,6 +4,7 @@
  */
 package entity;
 import adt.LinkedList;
+import java.time.LocalDate;
 /**
  *
  * @author haojuan
@@ -13,12 +14,24 @@ public class DonationManagement {
     private String donorID;
     private LinkedList<String> items;
     private String donationType;
+    private double amount;
+    private LocalDate donationDate;
 
     public DonationManagement(String donationID, String donorID, LinkedList<String> items, String donationType) {
         this.donationID = donationID;
         this.donorID = donorID;
         this.items = items;
         this.donationType = donationType;
+    }
+    
+    
+    public DonationManagement(String donationID, String donorID, LinkedList<String> items, String donationType, double amount, LocalDate donationDate) {
+        this.donationID = donationID;
+        this.donorID = donorID;
+        this.items = items;
+        this.donationType = donationType;
+        this.amount = amount;
+        this.donationDate = donationDate;
     }
 
     // Getters and Setters
@@ -53,10 +66,25 @@ public class DonationManagement {
     public void setDonationType(String donationType) {
         this.donationType = donationType;
     }
+        public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDonationDate() {
+        return donationDate;
+    }
+
+    public void setDonationDate(LocalDate donationDate) {
+        this.donationDate = donationDate;
+    }
+    
     
     @Override
     public String toString() {
-        return String.format("%5s, %15s, %15s, %15s", donationID, donorID, items, donationType);
-
+        return String.format("%5s, %15s, %15s, %15s, %10.2f, %s", donationID, donorID, items, donationType, amount, donationDate.toString());
     }
 }
