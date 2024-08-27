0bf9f21950a5ef11dd6baa32e15ba2126bf5263b
xuan <xuan@LAPTOP-5DH39D1R>
2024-08-28 00:45:26 +0800

    Merge origin/master
    # Conflicts:
    #	src/adt/MapInterface.java
    #	src/control/DonationManagement.java

--- src/boundary/driverUI.java
+++ src/boundary/driverUI.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package boundary;
+
 import java.util.*;
 
 /**
@@ -10,27 +11,27 @@ import java.util.*;
  * @author Yunshen
  */
 public class driverUI {
+
     Scanner scanner = new Scanner(System.in);
 
-  public int getMenuChoice() {
-   try{
-    System.out.println("\n******CHARITY MANAGEMENT SYSTEM******");
-    System.out.println("1. Donor Management");
-    System.out.println("2. Donee Management");
-    System.out.println("3. Donation Management");
-    System.out.println("4. Donation Distribution");
-    System.out.println("5. Volunteer Management");
-    System.out.println("0. Quit");
-    System.out.print("Enter choice(0-5): ");
-    int choice = scanner.nextInt();
-    scanner.nextLine();
-    System.out.println();
-    return choice;
-   }catch(Exception ex){
-       System.out.println("Invalid input");
-   }
-    return 0;
-  }
-   
+    public int getMenuChoice() {
+        try {
+            System.out.println("\n******CHARITY MANAGEMENT SYSTEM******");
+            System.out.println("1. Donor Management");
+            System.out.println("2. Donee Management");
+            System.out.println("3. Donation Management");
+            System.out.println("4. Donation Distribution");
+            System.out.println("5. Volunteer Management");
+            System.out.println("0. Quit");
+            System.out.print("Enter choice(0-5): ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
+            return choice;
+        } catch (Exception ex) {
+            System.out.println("Invalid input");
+        }
+        return 0;
+    }
 
 }
